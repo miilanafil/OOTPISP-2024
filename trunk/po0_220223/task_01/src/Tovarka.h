24e01c7b1f12ef927f141b29cb01e902e38cc5d4
milanafil <125284843+miilanafil@users.noreply.github.com>
2024-05-02 00:41:03 +0300

    сhange

--- trunk/po0_220223/task_01/src/Tovarka.h
+++ trunk/po0_220223/task_01/src/Tovarka.h
@@ -1,33 +1,32 @@
 #pragma once
 #ifndef TovarkaH
 #define TovarkaH
+
 #include <iostream>
-#include <cstring>
-#include <string> 
-const int len = 40;
+#include <string>
+
 class Imia
 {
     std::string name;
     int kol;
     int st;
+
 public:
-    Imia();
+    Imia() = default; // Конструктор по умолчанию
     Imia(const std::string_view, int, int);
-    Imia(const Imia&);
-    ~Imia();
+    Imia(const Imia&) = delete; // Удаление конструктора копирования
+    ~Imia() = default; // Деструктор по умолчанию
+
+    Imia& operator=(const Imia&) = delete; 
+
     std::string GetName() const;
     int GetKol() const;
     int GetSt() const;
     void SetName(const std::string_view);
     void SetKol(int);
     void SetSt(int);
-    void Set(const std::string_view Name, int Kol, int St);
-
-
+    void Set(const std::string_view, int, int);
     void Print() const;
-    
-    
 };
+
 #endif
-
-
