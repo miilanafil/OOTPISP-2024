62a04682ea0a179f5a778fbba7d36720575a2f38
redCat <yana.sitkowecz@yandex.ru>
2024-05-02 09:49:20 +0300

    Return spaces Exam.cpp

--- trunk/po0_220220/task_01/src/Exam.cpp
+++ trunk/po0_220220/task_01/src/Exam.cpp
@@ -2,31 +2,31 @@
 
 void Exam::SetName(const char *name)
 {
-    _studentName = std::make_unique<std::string>(name);
+	_studentName = std::make_unique<std::string>(name);
 }
 
 void Exam::Set(const char *name, const int date, const int grade)
 {
-    this->_date = date;
-    this->_grade = grade;
-    _studentName = std::make_unique<std::string>(name);
+	this->_date = date;
+	this->_grade = grade;
+	_studentName = std::make_unique<std::string>(name);
 }
 
 Exam::Exam(const char *name, const int date, const int grade)
-        : _studentName(std::make_unique<std::string>(name)), _date(date), _grade(grade)
+	: _studentName(std::make_unique<std::string>(name)), _date(date), _grade(grade)
 {
-    std::cout << "The constructor with parameters is called " << this << std::endl;
+	std::cout << "The constructor with parameters is called " << this << std::endl;
 }
 
 Exam::Exam()
 {
-    std::cout << "A constructor without parameters is called " << this << std::endl;
+	std::cout << "A constructor without parameters is called " << this << std::endl;
 }
 
 void Exam::Print() const
 {
-    std::cout << "Student name: " << _studentName << std::endl;
-    std::cout << "Exam day: " << _date << std::endl;
-    std::cout << "Grade: " << _grade << std::endl;
-    std::cout << std::endl;
+	std::cout << "Student name: " << _studentName << std::endl;
+	std::cout << "Exam day: " << _date << std::endl;
+	std::cout << "Grade: " << _grade << std::endl;
+	std::cout << std::endl;
 }
