c59ee138ed72ecba02c18ddac44d3b67da0b427e
milanafil <125284843+miilanafil@users.noreply.github.com>
2024-06-18 21:59:50 +0300

    change

--- trunk/po0_220223/task_04/src/Vvector.h
+++ trunk/po0_220223/task_04/src/Vvector.h
@@ -5,6 +5,16 @@
 #include <vector>
 
 template <typename T>
+class Vvector;
+
+// Декларация дружественных функций (без явной специализации в классе)
+template <class T>
+std::ostream &operator<<(std::ostream &out, const Vvector<T> &a);
+
+template <class T>
+std::istream &operator>>(std::istream &in, Vvector<T> &a);
+
+template <typename T>
 class Vvector
 {
 public:
@@ -15,13 +25,13 @@ public:
     int sizeVect() const;
     int operator()() const;
     T operator[](const int i) const;
-    auto operator+(const Vvector<T> &vect) const;
+    std::unique_ptr<Vvector<T>> operator+(const Vvector<T> &vect) const;
     void printVect() const;
 
     int getСontent() const { return content; };
 
-    friend std::ostream &operator<< <>(std::ostream &output, const Vvector<T> &a);
-    friend std::istream &operator>> <>(std::istream &input, Vvector<T> &a);
+    friend std::ostream &operator<< <T>(std::ostream &output, const Vvector<T> &a);
+    friend std::istream &operator>> <T>(std::istream &input, Vvector<T> &a);
 
 private:
     std::unique_ptr<std::vector<T>> vectorElmnt = std::make_unique<std::vector<T>>(2);
@@ -47,7 +57,7 @@ void Vvector<T>::addVect(const T &elmnt)
 }
 
 template <class T>
-auto Vvector<T>::operator+(const Vvector<T> &vect) const
+std::unique_ptr<Vvector<T>> Vvector<T>::operator+(const Vvector<T> &vect) const
 {
     auto newVector = std::make_unique<Vvector<T>>();
 
@@ -68,7 +78,7 @@ auto Vvector<T>::operator+(const Vvector<T> &vect) const
         newVector->addVect(vect[i]);
     }
 
-    return *newVector;
+    return newVector;
 }
 
 template <class T>
@@ -100,6 +110,7 @@ void Vvector<T>::printVect() const
     std::cout << "}" << std::endl;
 }
 
+// Определение дружественных функций (специализация шаблонов вне класса)
 template <class T>
 std::ostream &operator<<(std::ostream &out, const Vvector<T> &a)
 {
