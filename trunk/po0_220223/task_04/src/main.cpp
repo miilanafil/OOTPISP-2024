38a975ac4b9fb160f674e51d817e174db516b0f7
milanafil <125284843+miilanafil@users.noreply.github.com>
2024-06-18 22:21:37 +0300

    change

--- trunk/po0_220223/task_04/src/main.cpp
+++ trunk/po0_220223/task_04/src/main.cpp
@@ -7,8 +7,7 @@ int main()
     Vvector<int> vect1;
     Vvector<int> vect2;
 
-    std::cin >> vect1;
-    std::cout << vect1;
+    
     std::cout << vect1[1] << std::endl;
 
     vect2.addVect(1);
@@ -16,23 +15,21 @@ int main()
     vect2.addVect(3);
     vect2.addVect(4);
 
-    std::cout << vect2;
+   
     std::cout << vect2[2] << std::endl;
     std::cout << "size vector 1: " << vect1() << std::endl;
 
     auto sumVect = vect1 + vect2;
-    std::cout << *sumVect;
+   
 
     Vvector<Money> timeVect;
 
-    std::cin >> timeVect;
-    std::cout << timeVect;
-
+    
     timeVect.addVect(Money("11:41"));
     timeVect.addVect(Money("51:51"));
     timeVect.addVect(Money("31:50"));
     timeVect.addVect(Money("11:11"));
 
-    std::cout << timeVect;
+    
     return 0;
 }
