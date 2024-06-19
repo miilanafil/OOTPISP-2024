0ccc242e3c8b1ad2b9e289eed64403a7ad22fa14
milanafil <125284843+miilanafil@users.noreply.github.com>
2024-06-19 15:03:15 +0300

    change

--- trunk/po0_220223/task_05/src/Money.cpp
+++ trunk/po0_220223/task_05/src/Money.cpp
@@ -2,7 +2,7 @@
 
 Coins::Coins(double a)
     : rubles(static_cast<long>(a)),
-      kopeck(static_cast<int>((a - static_cast<long>(a)) * 100 + 0.5))
+      kopeck(static_cast<int>((a - static_cast<double>(rubles)) * 100 + 0.5))
 {
     // Здесь можно оставить тело конструктора пустым, если дополнительная логика не требуется
 }
